c42cd7bac804dd1c8f80c4d354f4d35430664705
Adam Lehavi <adamlehavi@gmail.com>
2024-11-11 17:35:36 -0800

    test on init

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -6,6 +6,3 @@ API
    :recursive:
 
    roveac
-   roveac/counter_checkers
-   roveac/dict_constructors
-   roveac/searches
--- roveac/__init__.py
+++ roveac/__init__.py
@@ -5,3 +5,21 @@ This package includes modules for counterexample checking and Ramsey graph analy
 """
 
 __version__ = "0.0.1"
+
+# From counter_checkers
+from .counter_checkers.counter_checker import CounterChecker  # adjust with the actual class or function names
+
+# From decrementors
+from .decrementors.decrementor import Decrementor
+
+# From dict_constructors
+from .dict_constructors.dict_constructor import DictConstructor
+
+# From isomorphism_hashers
+from .isomorphism_hashers.isomorphism_hasher import IsomorphismHasher
+
+# From key_generators
+from .key_generators.key_generator import KeyGenerator
+
+# From searches
+from .searches.search import Search
